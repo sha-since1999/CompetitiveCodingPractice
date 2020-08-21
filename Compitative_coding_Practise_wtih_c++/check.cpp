bd76c852166b495d10b2df691a7991ef8f175612
Rohit sahu <rohits8853@gmail.com>
2020-08-21 23:58:37 +0530

    check

--- Compitative_coding_Practise_wtih_c++/check.cpp
+++ Compitative_coding_Practise_wtih_c++/check.cpp
@@ -1,13 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    /*let's start sahu...*/
 
     return 0;
 }
