a627678fb999ba8533629bf546ac8e9d617458d6
Rohit sahu <rohits8853@gmail.com>
2020-08-21 23:48:42 +0530

    clear check file

--- Compitative_coding_Practise_wtih_c++/check.cpp
+++ Compitative_coding_Practise_wtih_c++/check.cpp
@@ -1,15 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
-void catalon(int n)
-{
-    ull dp[n + 1];
-    dp[0] = dp[1] = 1;
-    for (int i = 2; i <= n;i++)
-        for (int j = i; j <= n;j++)
-            dp[i] = dp[j] + dp[i-j-1];
 
-}
 int main()
 {
     ios::sync_with_stdio(false);
